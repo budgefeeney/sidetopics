b9bfda1b510e02261ee872307cbc93ce573c520e
Bryan Feeney <bryan.feeney@gmail.com>
2013-06-29 21:11:56 +0100

    Added initial skeleton

---
+++ src/model/sidetopic.py
@@ -0,0 +1,17 @@
+'''
+Created on 29 Jun 2013
+
+@author: bryanfeeney
+'''
+
+class SideTopic(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
