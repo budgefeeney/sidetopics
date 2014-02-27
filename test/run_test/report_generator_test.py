0a969e0df4a86e7ecd454b99a3578ad6edfadd3d
Bryan Feeney <bryan.feeney@gmail.com>
2014-02-27 15:20:01 +0000

    Very basic "test" (really just a test run) of the report generator

---
+++ test/run_test/report_generator_test.py
@@ -0,0 +1,28 @@
+'''
+Created on 27 Feb 2014
+
+@author: bryanfeeney
+'''
+import unittest
+
+import run.report_generator as rep_gen
+
+k5_results_dir = '/Users/bryanfeeney/Desktop/k5_results'
+
+class Test(unittest.TestCase):
+
+
+    def testGenerationOnRealCtmOutput(self):
+        cmdline = '' \
+                + ' --model '          + 'ctm' \
+                + ' --num-topics '     + '5,10,25,50' \
+                + ' --output-dir '     + k5_results_dir \
+                + ' --report-dir '     + k5_results_dir
+        
+        argv = cmdline.strip().split(' ')
+        rep_gen.run(argv)
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testGenerationOnRealReport']
+    unittest.main()
