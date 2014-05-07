47a67cd22059e6d95222d95fd0f0015997841ee6
Bryan Feeney <bryan.feeney@gmail.com>
2014-05-07 16:56:16 +0100

    Generate more reports

--- test/run_test/report_generator_test.py
+++ test/run_test/report_generator_test.py
@@ -7,8 +7,9 @@ import unittest
 
 import run.report_generator as rep_gen
 
-ResultsDir = '/Users/bryanfeeney/Desktop/tweets-dow/out'
-ReportsDir = '/Users/bryanfeeney/Desktop/tweets-dow/reports'
+ParentDir  = '/Users/bryanfeeney/Desktop/nips-out-f10-refs'
+ResultsDir = ParentDir + '/out'
+ReportsDir = ParentDir + '/reports'
 
 class Test(unittest.TestCase):
 
@@ -19,8 +20,9 @@ class Test(unittest.TestCase):
                 + ' --model '          + algor \
                 + ' --output-dir '     + ResultsDir \
                 + ' --report-dir '     + ReportsDir \
-                + ' --topic-list '     + '5,10,25,50,100,150,250' \
-                + ' --lat-sizes '      + '5,10,25,50,75,100'
+                + ' --topic-list '     + '5,10,25,50,100,150,250,350' \
+                + ' --lat-sizes '      + '5,10,25,50,75,100' \
+                + ' --dataset '        + rep_gen.NIPS
         
             argv = cmdline.strip().split(' ')
             rep_gen.run(argv)
