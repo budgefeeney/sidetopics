955c1a31d6d4da5f0743b427fea3a4aedd351f2a
Bryan Feeney <bryan.feeney@gmail.com>
2014-06-23 17:04:38 +0100

    Brought 32-bit impl in line with 64-bit impl

--- src/model/lda_vb_fast.pyx
+++ src/model/lda_vb_fast.pyx
@@ -97,76 +97,101 @@ def iterate_f32(int iterations, int D, int K, int T, \
     '''
     
     cdef:
-        int        itr, totalItrs, innerItrs, d, n, k, t
-        float[:]   oldMems       = np.ndarray(shape=(K,),  dtype=np.float32)
+        int         d, n, k, t
+        int         itr, innerItrs, totalItrs
         float[:,:] oldVocabDists = np.ndarray(shape=(K,T), dtype=np.float32)
         float[:,:] newVocabDists = vocabDists
-        float      norm    = 0.0
-        float      epsilon = 0.01
+        float[:]   oldMems       = np.ndarray(shape=(K,), dtype=np.float32)
+        float      max  = 1E-36
+        float      norm = 0.0
+        float[:]   vocabNorm = np.ndarray(shape=(K,), dtype=np.float32)
+        float      epsilon = 0.01 / K
         
-    with nogil:
-        totalItrs = 0
-        for itr in range(iterations):
-            oldVocabDists, newVocabDists = newVocabDists, oldVocabDists
-            newVocabDists[:,:] = 0.0
-            
+    totalItrs = 0
+    for itr in range(iterations):
+        oldVocabDists, newVocabDists = newVocabDists, oldVocabDists
+        
+        vocabNorm[:]       = vocabPrior * T
+        newVocabDists[:,:] = vocabPrior
+        
+        with nogil:
+        
+            # NOTE THIS CODE COPY AND PASTED INTO lda_vb.var_bound() !
             for d in range(D):
                 # For each document reset the topic probabilities and iterate to
                 # convergence. This means we don't have to store the per-token
-                # topic probabilties z_dnk for all documents, which is a huge structure
+                # topic probabilties z_dnk for all documents, which is a huge saving
                 oldMems[:]      = topicDists[d,:]
                 topicDists[d,:] = 1./K
-
                 innerItrs = 0
-                while (l1_dist_f32 (oldMems, topicDists[d,:]) > epsilon) and (innerItrs < MaxInnerItrs):
+                
+                while ((innerItrs < MinInnerIters) or (l1_dist_f32 (oldMems, topicDists[d,:]) > epsilon)) \
+                and (innerItrs < MaxInnerItrs):
+                    oldMems[:] = topicDists[d,:]
                     totalItrs += 1
                     innerItrs += 1
                     
+                    # Determine the topic assignment for each individual token...
                     for n in range(docLens[d]):
                         norm = 0.0
+                        max  = 1E-36
+                        
+                        # Work in log-space to avoid underflow
                         for k in range(K):
-                            z_dnk[n,k] = oldVocabDists[k,W_list[d,n]] * exp(digamma(topicDists[d,k]))
-                            if is_invalid(z_dnk[n,k]):
-                                with gil:
-                                    print ("Invalid probability value: i=%d:%d z[%d,%d,%d] = %f. exp(Psi(topicDists[%d,%d])) = exp(Psi(%f)) = exp(%f) = %f, oldVocabDists[k,W_list[d,n]] = oldVocabDists[%d,%d] = %f" % (itr, totalItrs, d, n, k, z_dnk[n,k], d, k, topicDists[d,k], digamma(topicDists[d,k]), exp(digamma(topicDists[d,k])), k, W_list[d,n], oldVocabDists[k,W_list[d,n]]))
-                                z_dnk[n,k] = 0
-                                
+                            z_dnk[n,k] = log(oldVocabDists[k,W_list[d,n]]) + digamma(topicDists[d,k])
+                            if z_dnk[n,k] > max:
+                                max = z_dnk[n,k]
+
+                        # Scale before converting to standard space so inference is feasible
+                        for k in range(K):
+                            z_dnk[n,k] = exp(z_dnk[n,k] - max)
                             norm += z_dnk[n,k]
-                            
+                     
+                        # Normalize the token probability, and check it's valid
                         for k in range(K):
                             z_dnk[n,k] /= norm
+                            if is_invalid(z_dnk[n,k]):
+                                with gil:
+                                    print ("Iteration %d:%d z_dnk[%d,%d] = %f, norm = %g" \
+                                           % (itr, totalItrs, n, k, z_dnk[n,k], norm))
 
+                    # Use all the individual word topic assignments to determine
+                    # the topic mixture exhibited by this document
+                    topicDists[d,:] = topicPrior
+                    norm = topicPrior * K
                     for n in range(docLens[d]):
-                        norm = 0.0
                         for k in range(K):
-                            topicDists[d,k] = topicPrior + z_dnk[n,k]
-                            norm += topicDists[d,k]
+                            topicDists[d,k] += z_dnk[n,k]
+                            norm += z_dnk[n,k]
                             
-                        for k in range(K):
-                            topicDists[d,k] /= norm
+                    for k in range(K):
+                        topicDists[d,k] /= norm        
                 
-                if innerItrs == MaxInnerItrs:
-                    with gil:
-                        print ("Iterated to max for document %d" % d)
-                            
-                
-                # Once converged, update the vocabulary distribution
+                # Once we've found document d's topic distribution, we
+                # use that to build the new vocabulary distribution
                 for k in range(K):
-                    norm = 0.0
                     for n in range(docLens[d]):
                         t = W_list[d,n]
                         newVocabDists[k,t] += z_dnk[n,k]
-                        norm += newVocabDists[k,t]
+                        vocabNorm[k] += z_dnk[n,k]
+                        
+                        if is_invalid(newVocabDists[k,t]):
+                            with gil:
+                                print ("newVocabDist[%d,%d] = %f, z_dnk[%d,%d] = %f" \
+                                      % (k, t, newVocabDists[k,t], n, k, z_dnk[n,k]))
                             
-                    for t in range(T):
-                        newVocabDists[k,t] /= norm
+            # With all document processed, normalize the vocabulary
+            for k in prange(K):
+                for t in range(T):
+                    newVocabDists[k,t] /= vocabNorm[k]
                 
         # Just before we return, make sure the vocabDists memoryview that
         # was passed in has the latest vocabulary distributions
         if iterations % 2 == 0:
             vocabDists[:,:] = newVocabDists
             
-    return totalItrs
+    print ("Average inner iterations %f" % (float(totalItrs) / (D*iterations)))
+    return totalItrs                        
                         
 
 @cython.boundscheck(False)
