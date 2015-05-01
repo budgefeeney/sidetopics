7cdfe4260d12a648569298917ef9dbbb664bee4c
Bryan Feeney <bryan.feeney@gmail.com>
2015-05-01 01:05:37 +0100

    Updated (mis-named) LDA/Gibbs test
    Now uses ACL data

--- test/model_test/lda_cvb_test.py
+++ test/model_test/lda_cvb_test.py
@@ -13,89 +13,90 @@ import matplotlib.cm as cm
 import pickle as pkl
 
 import model.lda_gibbs as lda
+from model.common import DataSet
+from model.evals import perplexity_from_like
+
+AclPath = "/Users/bryanfeeney/iCloud/Datasets/ACL/ACL.100/"
+AclWordPath = AclPath + "words-freq.pkl"
+AclCitePath = AclPath + "ref.pkl"
+AclDictPath = AclPath + "words-freq-dict.pkl"
 
 class Test(unittest.TestCase):
 
 
-    def testOnRealData(self):
+    def testPerplexityOnRealData(self):
         dtype = np.float64 # DTYPE
-        
+
         rd.seed(0xBADB055)
-        path = "/Users/bryanfeeney/Desktop/NIPS"
-        with open(path + "/ar.pkl", 'rb') as f:
-            _, W, _, d = pkl.load(f)
-            
-        if len(d) == 1:
-            d = d[0]
-        
-        W = W.astype(np.int32)
-        
-        docLens   = np.squeeze(np.asarray(W.sum(axis=1)))
-        good_rows = (np.where(docLens > 0.5))[0]
-        if len(good_rows) < W.shape[0]:
-            print ("Some rows in the doc-term matrix are empty. These have been removed.")
-        W = W[good_rows, :]
-        
+        data = DataSet(words = AclWordPath, links=AclCitePath)
+        with open(AclDictPath, "rb") as f:
+            d = pkl.load(f)
+
+        data.convert_to_dtype(np.int32)
+        data.prune_and_shuffle(min_doc_len=50, min_link_count=2)
+        data.convert_to_undirected_graph()
+        data.convert_to_binary_link_matrix()
+
         # IDF frequency for when we print out the vocab later
-        freq = np.squeeze(np.asarray(W.sum(axis=0)))
+        freq = np.squeeze(np.asarray(data.words.sum(axis=0)))
         scale = np.reciprocal(1 + freq)
-       
-        # Initialise the model  
+
+        # Initialise the model
         K = 10
-        
-        model      = lda.newModelAtRandom(W, K, dtype=np.float64)
-        queryState = lda.newQueryState(W, model)
-        trainPlan  = lda.newTrainPlan(iterations=1000, burnIn=300, thin=10)
-        
+        model      = lda.newModelAtRandom(data, K, dtype=dtype)
+        queryState = lda.newQueryState(data, model)
+        trainPlan  = lda.newTrainPlan(iterations=300, logFrequency=50, fastButInaccurate=False, debug=True)
+
         # Train the model, and the immediately save the result to a file for subsequent inspection
-        model, query, (bndItrs, bndVals, bndLikes) = lda.train (W, None, model, queryState, trainPlan)
+        model, query, (bndItrs, bndVals, bndLikes) = lda.train (data, model, queryState, trainPlan)
 #        with open(newModelFileFromModel(model), "wb") as f:
 #            pkl.dump ((model, query, (bndItrs, bndVals, bndLikes)), f)
-        
+
         # Plot the evolution of the bound during training.
-#         fig, ax1 = plt.subplots()
-#         ax1.plot(bndItrs, bndVals, 'b-')
-#         ax1.set_xlabel('Iterations')
-#         ax1.set_ylabel('Bound', color='b')
-#         
-#         ax2 = ax1.twinx()
-#         ax2.plot(bndItrs, bndLikes, 'r-')
-#         ax2.set_ylabel('Likelihood', color='r')
-#         
-#         fig.show()
-#         plt.show()
-        
-        vocab = model.vocabSum.copy() / model.numSamples
-#         plt.imshow(vocab, interpolation="none", cmap = cm.Greys_r)
-#         plt.show()
-        
-        print (str(model.vocabSum))
-            
+        fig, ax1 = plt.subplots()
+        ax1.plot(bndItrs, bndVals, 'b-')
+        ax1.set_xlabel('Iterations')
+        ax1.set_ylabel('Bound', color='b')
+
+        ax2 = ax1.twinx()
+        ax2.plot(bndItrs, bndLikes, 'r-')
+        ax2.set_ylabel('Likelihood', color='r')
+
+        fig.show()
+        plt.show()
+
+        vocab = lda.wordDists(model)
+        plt.imshow(vocab, interpolation="none", cmap = cm.Greys_r)
+        plt.show()
+
         # Print out the most likely topic words
-        topWordCount = 100
+        topWordCount = 10
         kTopWordInds = [self.topWordInds(d, vocab[k,:] * scale, topWordCount) \
                         for k in range(K)]
-        
-        print(str(model.topicPrior))
-        print("%6.3f  %6.3f  %6.3f" % (model.vocabPrior.min(), model.vocabPrior.mean(), model.vocabPrior.max()))
-        
-#         print ("Perplexity: %f\n\n" % lda.perplexity(W, model, query))
-        print ("\t".join (["%20s" % ("Topic " + str(k)) for k in range(K)]))
-        print ("\n".join ("\t".join ("%20s\t%0.3f" % (d[kTopWordInds[k][c]], vocab[k][kTopWordInds[k][c]]) for k in range(K)) for c in range(topWordCount)))
-        
+
+        like = lda.log_likelihood(data, model, query)
+        perp = perplexity_from_like(like, data.word_count)
+
+        print ("Prior %s" % (str(model.topicPrior)))
+        print ("Perplexity: %f\n\n" % perp)
+
+        for k in range(model.K):
+            print ("\nTopic %d\n=============================" % k)
+            print ("\n".join("%-20s\t%0.4f" % (d[kTopWordInds[k][c]], vocab[k][kTopWordInds[k][c]]) for c in range(topWordCount)))
+
+
     def topWords (self, wordDict, vocab, count=10):
         return [wordDict[w] for w in self.topWordInds(wordDict, vocab, count)]
 
-    
+
     def topWordInds (self, wordDict, vocab, count=10):
         return vocab.argsort()[-count:][::-1]
-    
+
     def printTopics(self, wordDict, vocab, count=10):
         words = vocab.argsort()[-count:][::-1]
         for wordIdx in words:
             print("%s" % wordDict[wordIdx])
         print("")
-
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
