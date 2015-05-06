a4d665d9de5676961e2101da7c342051d801daac
Bryan Feeney <bryan.feeney@gmail.com>
2015-05-06 20:17:05 +0100

    Add MAP unit test

--- test/model_test/rtm_test.py
+++ test/model_test/rtm_test.py
@@ -14,17 +14,36 @@ import pickle as pkl
 
 import model.rtm as rtm
 from model.common import DataSet
-from model.evals import perplexity_from_like
+from model.evals import perplexity_from_like, mean_average_prec
 
 AclPath = "/Users/bryanfeeney/iCloud/Datasets/ACL/ACL.100/"
 AclWordPath = AclPath + "words-freq.pkl"
 AclCitePath = AclPath + "ref.pkl"
 AclDictPath = AclPath + "words-freq-dict.pkl"
 
-class Test(unittest.TestCase):
+MinDocLen = 50
+MinLinkCount = 8
+Iters = 200
+LogFreq = 5
+TopicCount = 10
+
+class RtmTest(unittest.TestCase):
+
+    def topWords (self, wordDict, vocab, count=10):
+        return [wordDict[w] for w in self.topWordInds(wordDict, vocab, count)]
 
 
-    def testPerplexityOnRealData(self):
+    def topWordInds (self, vocab, count=10):
+        return vocab.argsort()[-count:][::-1]
+
+    def printTopics(self, wordDict, vocab, count=10):
+        words = vocab.argsort()[-count:][::-1]
+        for wordIdx in words:
+            print("%s" % wordDict[wordIdx])
+        print("")
+
+
+    def _testPerplexityOnRealData(self):
         dtype = np.float64 # DTYPE
 
         rd.seed(0xBADB055)
@@ -33,19 +52,19 @@ class Test(unittest.TestCase):
             d = pkl.load(f)
 
         data.convert_to_dtype(dtype)
-        data.prune_and_shuffle(min_doc_len=50, min_link_count=2)
         data.convert_to_undirected_graph()
         data.convert_to_binary_link_matrix()
+        data.prune_and_shuffle(min_doc_len=MinDocLen, min_link_count=MinLinkCount)
 
         # IDF frequency for when we print out the vocab later
         freq = np.squeeze(np.asarray(data.words.sum(axis=0)))
         scale = np.reciprocal(1 + freq)
 
         # Initialise the model
-        K = 10
+        K = TopicCount
         model      = rtm.newModelAtRandom(data, K, dtype=dtype)
         queryState = rtm.newQueryState(data, model)
-        trainPlan  = rtm.newTrainPlan(iterations=40, logFrequency=5, fastButInaccurate=False, debug=True)
+        trainPlan  = rtm.newTrainPlan(iterations=Iters, logFrequency=LogFreq, fastButInaccurate=False, debug=True)
 
         # Train the model, and the immediately save the result to a file for subsequent inspection
         model, query, (bndItrs, bndVals, bndLikes) = rtm.train (data, model, queryState, trainPlan)
@@ -65,12 +84,9 @@ class Test(unittest.TestCase):
         fig.show()
         plt.show()
 
-        vocab = rtm.wordDists(model)
-        plt.imshow(vocab, interpolation="nearest", cmap = cm.Greys_r)
-        plt.show()
-
         # Print out the most likely topic words
         # scale = np.reciprocal(1 + np.squeeze(np.array(data.words.sum(axis=0))))
+        vocab = rtm.wordDists(model)
         topWordCount = 10
         kTopWordInds = [self.topWordInds(vocab[k,:], topWordCount) for k in range(K)]
 
@@ -85,19 +101,69 @@ class Test(unittest.TestCase):
             print ("\n".join("%-20s\t%0.4f" % (d[kTopWordInds[k][c]], vocab[k][kTopWordInds[k][c]]) for c in range(topWordCount)))
 
 
-    def topWords (self, wordDict, vocab, count=10):
-        return [wordDict[w] for w in self.topWordInds(wordDict, vocab, count)]
+    def testMapOnRealData(self):
+        dtype = np.float64 # DTYPE
+
+        rd.seed(0xBADB055)
+        data = DataSet.from_files(words_file=AclWordPath, links_file=AclCitePath)
+        with open(AclDictPath, "rb") as f:
+            d = pkl.load(f)
+
+        data.convert_to_dtype(dtype)
+        data.convert_to_undirected_graph()
+        data.convert_to_binary_link_matrix()
+        data.prune_and_shuffle(min_doc_len=MinDocLen, min_link_count=MinLinkCount)
+
+        trainData, testData = data.doc_completion_split()
+
+        # Initialise the model
+        K = TopicCount
+        model      = rtm.newModelAtRandom(trainData, K, dtype=dtype)
+        queryState = rtm.newQueryState(trainData, model)
+        trainPlan  = rtm.newTrainPlan(iterations=2, logFrequency=LogFreq, fastButInaccurate=False, debug=True)
+
+        # Train the model, and the immediately save the result to a file for subsequent inspection
+        model, topics, (bndItrs, bndVals, bndLikes) = rtm.train(trainData, model, queryState, trainPlan)
+#        with open(newModelFileFromModel(model), "wb") as f:
+#            pkl.dump ((model, query, (bndItrs, bndVals, bndLikes)), f)
+
+        # Plot the evolution of the bound during training.
+        fig, ax1 = plt.subplots()
+        ax1.plot(bndItrs, bndVals, 'b-')
+        ax1.set_xlabel('Iterations')
+        ax1.set_ylabel('Bound', color='b')
+
+        ax2 = ax1.twinx()
+        ax2.plot(bndItrs, bndLikes, 'r-')
+        ax2.set_ylabel('Likelihood', color='r')
+
+        fig.show()
+        plt.show()
+
+        # Print out the most likely topic words
+        # scale = np.reciprocal(1 + np.squeeze(np.array(data.words.sum(axis=0))))
+        vocab = rtm.wordDists(model)
+        topWordCount = 10
+        kTopWordInds = [self.topWordInds(vocab[k,:], topWordCount) for k in range(K)]
+
+        like = rtm.log_likelihood(trainData, model, topics)
+        perp = perplexity_from_like(like, trainData.word_count)
+
+        print ("Prior %s" % (str(model.topicPrior)))
+        print ("Train Perplexity: %f\n\n" % perp)
+
+        for k in range(model.K):
+            print ("\nTopic %d\n=============================" % k)
+            print ("\n".join("%-20s\t%0.4f" % (d[kTopWordInds[k][c]], vocab[k][kTopWordInds[k][c]]) for c in range(topWordCount)))
+
+        min_probs  = rtm.min_link_probs(model, topics, testData.links)
+        link_probs = rtm.link_probs(model, topics, min_probs)
+        map = mean_average_prec(testData.links, link_probs)
+
+        print("The Mean-Average-Precision is %.3f" % map)
 
 
-    def topWordInds (self, vocab, count=10):
-        return vocab.argsort()[-count:][::-1]
-
-    def printTopics(self, wordDict, vocab, count=10):
-        words = vocab.argsort()[-count:][::-1]
-        for wordIdx in words:
-            print("%s" % wordDict[wordIdx])
-        print("")
-
+#
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
