4c965d9aa1a00de61036a8ba7e82e45f7a87e567
Bryan Feeney <bryan.feeney@gmail.com>
2015-07-02 14:31:25 +0100

    Use numba again

--- src/model/lda_vb_python.py
+++ src/model/lda_vb_python.py
@@ -8,7 +8,7 @@ import numpy as np
 import numpy.random as rd
 import scipy.linalg as la
 import scipy.special as fns
-#import numba as nb
+import numba as nb
 
 
 from util.sparse_elementwise import sparseScalarProductOfSafeLnDot
@@ -79,7 +79,7 @@ def newModelAtRandom(data, K, topicPrior=None, vocabPrior=None, dtype=DTYPE):
     if topicPrior is None:
         topicPrior = constantArray((K,), 5.0 / K + 0.5, dtype) # From Griffiths and Steyvers 2004
     if vocabPrior is None:
-        vocabPrior = 5 # Also from G&S
+        vocabPrior = 1.1 # Also from G&S
 
     wordDists = np.ones((K,T), dtype=dtype)
     for k in range(K):
@@ -201,7 +201,7 @@ def _convertMeansToDirichletParam(docLens, topicMeans, topicPrior):
     topicMeans += topicPrior[np.newaxis, :]
     return topicMeans
 
-#@nb.autojit
+@nb.autojit
 def _inplace_softmax_colwise(z):
     '''
     Softmax transform of the given vector of scores into a vector of
@@ -224,7 +224,7 @@ def _inplace_softmax_colwise(z):
     z_sum = z.sum(axis=0)
     z /= z_sum[np.newaxis, :]
 
-#@nb.autojit
+@nb.autojit
 def _inplace_softmax_rowwise(z):
     '''
     Softmax transform of the given vector of scores into a vector of
@@ -246,7 +246,7 @@ def _inplace_softmax_rowwise(z):
 
 
 
-#@nb.autojit
+@nb.autojit
 def _update_topics_at_d(d, data, docLens, topicMeans, topicPrior, diWordDists, diWordDistSums):
     '''
     Infers the topic assignments for all present words in the given document at
@@ -271,7 +271,7 @@ def _update_topics_at_d(d, data, docLens, topicMeans, topicPrior, diWordDists, d
     topicMeans[d, :] = np.dot(z, data.words[d, :].data) / docLens[d]
     return wordIdx, z
 
-#@nb.autojit
+@nb.autojit
 def _infer_topics_at_d(d, data, docLens, topicMeans, topicPrior, diWordDists, diWordDistSums):
     '''
     Infers the topic assignments for all present words in the given document at
@@ -307,7 +307,7 @@ def _infer_topics_at_d(d, data, docLens, topicMeans, topicPrior, diWordDists, di
     return wordIdx, z
 
 
-#@nb.autojit
+@nb.autojit
 def train(data, model, query, plan, updateVocab=True):
     '''
     Infers the topic distributions in general, and specifically for
@@ -376,7 +376,7 @@ def train(data, model, query, plan, updateVocab=True):
                 perp = perplexity_from_like(likes[-1], W.sum())
                 print ("Iteration %d : Train Perp = %4.0f  Bound = %.3f" % (itr, perp, bnds[-1]))
 
-                if len(iters) > 2:
+                if len(iters) > 2: #and iters[-1] > 100:
                     lastPerp = perplexity_from_like(likes[-2], W.sum())
                     if lastPerp - perp < 1:
                         break;
@@ -474,7 +474,7 @@ def printAndFlushNoNewLine(text):
 
 
 
-##@nb.autojit
+#@nb.autojit
 def query(data, model, query, plan):
     '''
     Infers the topic distributions in general, and specifically for
@@ -497,7 +497,7 @@ def query(data, model, query, plan):
     _, topics, (_,_,_) = train(data, model, query, plan, updateVocab=False)
     return model, topics
 
-#@nb.autojit
+@nb.autojit
 def _var_bound_internal(data, model, query, z_dnk = None):
     _convertMeansToDirichletParam(query.docLens, query.topicDists, model.topicPrior)
     result = var_bound(data, model, query, z_dnk)
@@ -506,7 +506,7 @@ def _var_bound_internal(data, model, query, z_dnk = None):
     return result
 
 
-#@nb.autojit
+@nb.autojit
 def var_bound(data, model, query, z_dnk = None):
     '''
     Determines the variational bounds.
