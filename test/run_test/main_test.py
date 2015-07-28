8bc089f49f92bd1fff7c274dba106989fcbe9ee7
Bryan Feeney <bryan.feeney@gmail.com>
2015-07-28 21:19:30 +0100

    Add a few more Twitter dataset

--- test/run_test/main_test.py
+++ test/run_test/main_test.py
@@ -11,25 +11,38 @@ import cProfile
 from model_test.stm_yv_test import sampleFromModel
 from run.main import run, ModelNames, \
     Rtm, LdaGibbs, LdaVb, Mtm, StmYvBohning, StmYvBouchard, CtmBohning, CtmBouchard
-from model.evals import Perplexity, MeanAveragePrecAllDocs, MeanPrecRecAtMAllDocs
+from model.evals import Perplexity, MeanAveragePrecAllDocs, MeanPrecRecAtMAllDocs, HashtagPrecAtM
 
 AclPath = "/Users/bryanfeeney/iCloud/Datasets/ACL/ACL.100.clean/"
 _AclWordPath  = AclPath + "words-freq.pkl"
+_AclDictPath  = AclPath + "words-freq-dict.pkl"
 _AclFeatsPath = AclPath + "feats.pkl"
 _AclCitePath  = AclPath + "ref.pkl"
 
 NipsPath = "/Users/bryanfeeney/iCloud/Datasets/NIPS-from-pryor-Sep15/"
 _NipsWordPath = NipsPath + "W_ar.pkl"
 _NipsFeatPath = NipsPath + "X_ar.pkl"
+_NipsDictPath = None
 
 
-Tweets2500Path = "/Users/bryanfeeney/iCloud/Datasets/Tweets/Tweets-2.5m/"
-_Tweets2500WordPath = Tweets2500Path + "words.pkl"
-_Tweets2500FeatPath = Tweets2500Path + "side.pkl"
+Tweets1100Path = "/Users/bryanfeeney/iCloud/Datasets/Tweets/Tweets-1.1m/"
+_Tweets1100WordPath = Tweets1100Path + "words.pkl"
+_Tweets1100FeatPath = Tweets1100Path + "side.pkl"
 
-_AuthorTweets2500WordPath     = Tweets2500Path + "words-by-author.pkl"
-_AuthorTweets2500FreqWordPath = Tweets2500Path + "words-by-author-freq.pkl"
-_Tweets2500FreqWordPath       = Tweets2500Path + "words-freq.pkl"
+_AuthorTweets1100WordPath     = Tweets1100Path + "words-by-author.pkl"
+_AuthorTweets1100FreqWordPath = Tweets1100Path + "words-by-author-freq.pkl"
+_Tweets1100FreqWordPath       = Tweets1100Path + "words-freq.pkl"
+_Tweets1100FreqDictPath       = Tweets1100Path + "words-freq-dict.pkl"
+
+
+Tweets2900Path = "/Users/bryanfeeney/iCloud/Datasets/Tweets/Tweets-2.9m/"
+_Tweets2900WordPath = Tweets2900Path + "words.pkl"
+_Tweets2900FeatPath = Tweets2900Path + "side.pkl"
+
+_AuthorTweets2900WordPath     = Tweets2900Path + "words-by-author.pkl"
+_AuthorTweets2900FreqWordPath = Tweets2900Path + "words-by-author-freq.pkl"
+_Tweets2900FreqWordPath       = Tweets2900Path + "words-freq.pkl"
+_Tweets2900FreqDictPath       = Tweets2900Path + "words-freq-dict.pkl"
 
 
 Tweets750Path = "/Users/bryanfeeney/iCloud/Datasets/Tweets/Cluster2015-06-24/AuthorTime750/"
@@ -39,6 +52,7 @@ _Tweets750FeatPath = Tweets750Path + "side-cleaned.pkl"
 _AuthorTweets750WordPath     = Tweets750Path + "words-by-author.pkl"
 _AuthorTweets750FreqWordPath = Tweets750Path + "words-by-author-freq.pkl"
 _Tweets750FreqWordPath       = Tweets750Path + "words-cleaned-freq.pkl"
+_Tweets750FreqDictPath       = Tweets750Path + "words-cleaned-freq-dict.pkl"
 
 Tweets800Path = "/Users/bryanfeeney/Desktop/SmallerDB-NoCJK-WithFeats-Fixed/"
 _Tweets800WordPath = Tweets800Path + "words.pkl"
@@ -47,6 +61,7 @@ _Tweets800FeatPath = Tweets800Path + "side.pkl"
 _AuthorTweets800WordPath     = Tweets800Path + "words-by-author.pkl"
 _AuthorTweets800FreqWordPath = Tweets800Path + "words-by-author-freq.pkl"
 _Tweets800FreqWordPath       = Tweets800Path + "words-freq.pkl"
+_Tweets800FreqWordPath       = Tweets800Path + "words-freq-dict.pkl"
 
 Tweets500Path = "/Users/bryanfeeney/iCloud/Datasets/Tweets/AuthorTime/"
 _Tweets500WordPath = Tweets500Path + "words.pkl"
@@ -55,20 +70,23 @@ _Tweets500FeatPath = Tweets500Path + "side.pkl"
 _Author500TweetsWordPath     = Tweets500Path + "words-by-author.pkl"
 _Author500TweetsFreqWordPath = Tweets500Path + "words-by-author-freq.pkl"
 _Tweets500FreqWordPath       = Tweets500Path + "words-cleaned-freq.pkl"
+_Tweets500FreqDictPath       = Tweets500Path + "words-cleaned-freq-dict.pkl"
 
- # Pick either 500 or 750 or 800 or 2500
-_TweetsWordPath = _Tweets2500WordPath
-_TweetsFeatPath = _Tweets2500FeatPath
+ # Pick either 500 or 750 or 800 or 1100 or 2900
+_TweetsWordPath = _Tweets500WordPath
+_TweetsFeatPath = _Tweets500FeatPath
 
-_AuthorTweetsWordPath     = _AuthorTweets2500WordPath
-_AuthorTweetsFreqWordPath = _AuthorTweets2500FreqWordPath
-_TweetsFreqWordPath       = _Tweets2500FreqWordPath
+_AuthorTweetsWordPath     = _Author500TweetsWordPath
+_AuthorTweetsFreqWordPath = _Author500TweetsFreqWordPath
+_TweetsFreqWordPath       = _Tweets500FreqWordPath
+_TweetsFreqDictPath       = _Tweets500FreqDictPath
 
 
 Acl, AclNoLinks, TweetsAll, TweetsFreq, AuthorTweetsAll, AuthorTweetsFreq, Nips = 0, 1, 2, 3, 4, 5, 6
 WordsPath = [_AclWordPath,  _AclWordPath,  _TweetsWordPath, _TweetsFreqWordPath, _AuthorTweetsWordPath, _AuthorTweetsFreqWordPath, _NipsWordPath]
-FeatsPath = [_AclFeatsPath, _AclFeatsPath, _TweetsFeatPath, _TweetsFeatPath,     None,                 None,                     _NipsFeatPath]
-CitesPath = [_AclCitePath,  None,         None,           None,               None,                 None,                     None]
+FeatsPath = [_AclFeatsPath, _AclFeatsPath, _TweetsFeatPath, _TweetsFeatPath,     None,                  None,                      _NipsFeatPath]
+CitesPath = [_AclCitePath,  None,          None,            None,                None,                  None,                      None]
+DictsPath = [_AclDictPath,  _AclDictPath,  None,            _TweetsFreqDictPath, None,                  _TweetsFreqDictPath,       _NipsDictPath]
 
 def tmpFiles():
     '''
@@ -104,7 +122,7 @@ class Test(unittest.TestCase):
 
         Folds, ExecutedFoldCount = 5, 1
         K,P = 25, 50
-        TrainIters, QueryIters, LogFreq = 2000, 500, 10
+        TrainIters, QueryIters, LogFreq = 5000, 500, 10
         PriorCov = 0.001
         VocabPrior = 0.01
         Debug = False
@@ -116,15 +134,16 @@ class Test(unittest.TestCase):
                     cmdline = '' \
                             + (' --debug '         + str(Debug) if Debug else "") \
                             + ' --model '          + modelName \
-                            + ' --dtype '          + 'f8:f8'      \
+                            + ' --dtype '          + 'f4:f4'      \
                             + ' --num-topics '     + str(k)    \
                             + ' --num-lat-feats '  + str(P) \
                             + ' --log-freq '       + str(LogFreq)       \
-                            + ' --eval '           + 'perplexity'  \
+                            + ' --eval '           + HashtagPrecAtM  \
                             + ' --iters '          + str(TrainIters)      \
                             + ' --query-iters '    + str(QueryIters)      \
                             + ' --folds '          + str(Folds)      \
                             + ' --truncate-folds ' + str(ExecutedFoldCount)      \
+                            + (' --word-dict '     + DictsPath[DataSetName] if DictsPath[DataSetName] is not None else "") \
                             + ' --words '          + WordsPath[DataSetName] \
                             + (' --feats '         + FeatsPath[DataSetName] if FeatsPath[DataSetName] is not None else "") \
                             + (' --links '         + CitesPath[DataSetName] if CitesPath[DataSetName] is not None else "") \
